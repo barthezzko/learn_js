e60a4a7d3c64e0491056435b7803eb92a6e0257e
bzpc\barthezzko <barthezzko@gmail.com>
2017-07-19 12:23:47 +0300

    updates for testing

---
+++ express_app/test/data_types.js
@@ -0,0 +1,66 @@
+var assert = require('assert');
+describe('Data types', function () {
+    var obj1 = {};
+    var obj2 = {};
+    var prim1 = 123;
+    var prim2 = 123;
+
+    it('two distinct objects are not strictly equal', function () {
+        assert.equal(false, obj1 === obj2);
+    });
+    it('two primitives are strictly equal', function () {
+        assert.equal(true, prim1 === prim2);
+    });
+
+    it('primitives', function () {
+        assert.equal('string', typeof 'asdf');
+        assert.equal('boolean', typeof true);
+        assert.equal('number', typeof 1234);
+        assert.equal('undefined', typeof undefined);
+        assert.equal('object', typeof null);
+        assert.equal('function', typeof function () {});
+
+    });
+    it('objects', function () {
+        var simpleObj = {'prop':'prop value'};
+        assert.equal('prop value', simpleObj.prop);
+        simpleObj.newProp = true;
+        assert.equal(true, simpleObj.newProp);
+        delete simpleObj.newProp;
+        assert.equal(undefined, simpleObj.newProp);
+    });
+    it('undefined and null', function () {
+        var obj = {prop:42};
+        assert.equal(42, obj.prop);
+        assert.equal(undefined, obj.prop1);
+        function f(a,b,c){
+            console.log('inside f()')
+            assert.equal(1, a);
+            assert.equal(undefined, b);
+            assert.equal(undefined, c);
+        }
+        f(1);
+
+    });
+    it('falsy values', function(){
+        var falsyValues = [false, 0, NaN, ''];
+        for (k in falsyValues) {
+            console.log('checking for value: ' + falsyValues[k]);
+            if (falsyValues[k]) {
+                assert.fail(falsyValues[k] + ' is falsy value');
+            }
+        }
+
+    });
+    it('typeof vs instanceof', function () {
+        function Bar(){
+
+        }
+        assert.equal('object', typeof {});
+        assert.equal('object', typeof []);
+        assert.equal(true, new Bar() instanceof Bar);
+        assert.equal(true, [] instanceof Array);
+        assert.equal(false, [] instanceof Bar);
+        assert.equal(true, [] instanceof Object);
+    });
+});
--- express_app/test/test_simple.js
+++ express_app/test/test_simple.js
@@ -4,5 +4,10 @@ describe('Array', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal(-1, [1,2,3].indexOf(4));
         });
+   });
+    describe('simple properties', function() {
+        it('string length should be 3', function() {
+            assert.equal(3, 'abc'.length);
+        });
     });
 });
